d792585dbdc9ccf24452654ecc6f17f02e2c587a
heian <minayeh1347@gmail.com>
2026-07-03 20:07:56 +0800

    all files in

---
+++ Firmware/mp3.cpp
@@ -0,0 +1,37 @@
+//I2S
+#define DIN 27
+#define LRCLK 25
+#define BCLK 26
+#define LEFT 4
+#define RIGHT 32
+#define GAIN 2
+#define GAIN0 16
+#define GAIN1 17
+
+//I2C
+#define SDA 21
+#define SCL 22
+
+//SD (SPI)
+#define CS 5
+#define MOSI 23
+#define MISO 19
+#define CLK 18
+
+//UART
+#define TXD 1
+#define RXD 3
+
+//BOOT
+#define BOOT 0  
+
+//Rotary Encoder (MENU)
+#define UP 14
+#define DOWN 13
+#define SELECT 15
+
+//Rotary Encoder (VOLUME)
+#define VOLUME_UP 34
+#define VOLUME_DOWN 33
+#define VOLUME_SELECT 35
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # miniMP3
 
-##
+## About
+
+A compact music player. 59.3 x 32 x 27mm
